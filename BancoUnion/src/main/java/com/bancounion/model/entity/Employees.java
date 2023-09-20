b46efead380d66e0b120e641efd0a921a50a391b
Jefferson Moreno <alexdex95@gmail.com>
2023-09-19 20:31:54 -0500

    se incluye libreria jwt

--- BancoUnion/src/main/java/com/bancounion/model/entity/Employees.java
+++ BancoUnion/src/main/java/com/bancounion/model/entity/Employees.java
@@ -1,11 +1,9 @@
 package com.bancounion.model.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "Employees")
